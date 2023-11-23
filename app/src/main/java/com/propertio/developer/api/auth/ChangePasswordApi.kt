685c424d1dd99e7ae893ccf344ee810a9cfd4451
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-23 12:15:00 +0700

    feat(profile) : change password not fix

--- app/src/main/java/com/propertio/developer/api/auth/ChangePasswordApi.kt
+++ app/src/main/java/com/propertio/developer/api/auth/ChangePasswordApi.kt
@@ -4,17 +4,9 @@ import com.propertio.developer.api.models.ChangePasswordRequest
 import com.propertio.developer.api.models.ChangePasswordResponse
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
-interface UserApi {
-
-    @POST("v1/auth/login")
-    fun login(
-        @Body userRequest: UserRequest
-    ): Call<UserResponse>
-
+interface ChangePasswordApi {
     @POST("/v1/reset-password")
     fun changePassword(@Body request: ChangePasswordRequest): Call<ChangePasswordResponse>
-
 }
