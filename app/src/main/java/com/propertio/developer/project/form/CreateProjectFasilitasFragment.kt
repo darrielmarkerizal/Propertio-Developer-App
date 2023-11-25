6dd700e0b463b4b4b94269a234a500f0359e9ea2
xvqiuu <132545168+xvqiuu@users.noreply.github.com>
2023-11-25 10:46:12 +0700

    feat(project) : interface form project fasilitas and infrastruktur

--- app/src/main/java/com/propertio/developer/project/form/CreateProjectFasilitasFragment.kt
+++ app/src/main/java/com/propertio/developer/project/form/CreateProjectFasilitasFragment.kt
@@ -1,26 +1,26 @@
 package com.propertio.developer.project.form
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.propertio.developer.databinding.FragmentCreateProjectInfrastrukturBinding
+import com.propertio.developer.databinding.FragmentCreateProjectFasilitasBinding
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CreateProjectInfrastrukturFragment.newInstance] factory method to
+ * Use the [CreateProjectFasilitasFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CreateProjectInfrastrukturFragment : Fragment() {
+class CreateProjectFasilitasFragment : Fragment() {
 
     private val binding by lazy {
-        FragmentCreateProjectInfrastrukturBinding.inflate(layoutInflater)
+        FragmentCreateProjectFasilitasBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -30,4 +30,5 @@ class CreateProjectInfrastrukturFragment : Fragment() {
         // Inflate the layout for this fragment
         return binding.root
     }
+
 }
