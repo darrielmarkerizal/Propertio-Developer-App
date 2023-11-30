05d95623bf62582b765e6d0fd628df6bb47fd770
ergegananputra <ergegananputra@gmail.com>
2023-11-30 11:17:11 +0700

    Profile
    New:
    + New! Register Feature
    
    Developer Information :
    + API Unitmasih belum beres, jadi belum dibuat fetching datanya
    
    Known Bugs:
    + Ketika menekan menu message kemudian user menekan menu project dengan cepat maka menu list project tidak akan tertampil
    + Ketika list pesan baru saja ke load dan user dengan cepat menekan salah satu pesan maka akan crash
    + Spinner belum bekerja, Upload foto belum, foto profil belum, upload banner belum, logout belum bekerja

---
+++ app/src/main/java/com/propertio/developer/database/profile/ProfileTable.kt
@@ -0,0 +1,37 @@
+package com.propertio.developer.database.profile
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "profile_table")
+data class ProfileTable (
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    var id: Int = 0,
+
+
+    @ColumnInfo(name = "id_user")
+    var idUser: String? = null,
+
+    @ColumnInfo(name = "email")
+    var email: String? = null,
+
+    @ColumnInfo(name = "name")
+    var name: String? = null,
+
+    @ColumnInfo(name = "phone")
+    var phone: String? = null,
+
+    @ColumnInfo(name = "province")
+    var province: String? = null,
+
+    @ColumnInfo(name = "city")
+    var city: String? = null,
+
+    @ColumnInfo(name = "address")
+    var address: String? = null,
+)
