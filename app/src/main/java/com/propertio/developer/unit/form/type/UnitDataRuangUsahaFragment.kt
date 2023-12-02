5d8331e5c6e35b517b11ffad467337b28b22d22f
ergegananputra <ergegananputra@gmail.com>
2023-12-02 21:15:58 +0700

    Move Unit
    New:
    + New! Form Create Project Informasi Umum and Create Project Lokasi
    
    Known Bugs:
    + Ketika menekan menu message kemudian user menekan menu project dengan cepat maka menu list project tidak akan tertampil
    + Ketika list pesan baru saja ke load dan user dengan cepat menekan salah satu pesan maka akan crash

---
+++ app/src/main/java/com/propertio/developer/unit/form/type/UnitDataRuangUsahaFragment.kt
@@ -0,0 +1,27 @@
+package com.propertio.developer.unit.form.type
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.propertio.developer.R
+
+
+class UnitDataRuangUsahaFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_unit_data_ruang_usaha, container, false)
+    }
+
+
+}
